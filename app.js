795340facd8f194b465e3f69d83187f31b88c09c
DaniPinelli <elidaniypri@gmail.com>
2020-09-16 16:50:29 -0300

    alpha1

---
+++ app.js
@@ -0,0 +1,25 @@
+//Requerimos todo lo que vamos a usar
+//Requerimos express
+const express = require('express');
+//Instanciamos express
+const app = express();
+//Módulo (path) nativo de node para trabajar con rutas de archivos
+const path = require ('path');
+
+//Ruta raíz
+app.get('/', (req, res) => {
+    let routeFile = path.resolve('./Vistas/index.html');
+    res.sendFile(routeFile);
+}); 
+
+// Ruta para archivos
+app.get('*', (req, res) => { 
+    if(req.url.includes('.')){
+        let file = path.resolve('Public' + req.url)
+        return res.sendFile(file)
+    };
+    res.sendFile('404 Not Found');
+})
+//http://localhost:3000/
+app.listen(3000, () => console.log('Server running ok on port 3000'));
+
