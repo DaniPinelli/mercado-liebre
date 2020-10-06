0d8e60eb8a65192e1864daa5a4aac6337312f38a
DaniPinelli <elidaniypri@gmail.com>
2020-10-06 20:33:24 -0300

    Modificada la forma de asignar los estilos

--- app.js
+++ app.js
@@ -4,32 +4,31 @@ const express = require('express');
 //Instanciamos express
 const app = express();
 //Módulo (path) nativo de node para trabajar con rutas de archivos
-const path = require ('path');
+const path = require('path');
 
 //Ruta raíz
 app.get('/', (req, res) => {
-    let routeFile = path.resolve('./Vistas/index.html');
+    let routeFile = path.resolve('Vistas/index.html');
     res.sendFile(routeFile);
-}); 
+});
 
 app.get('/registrer', (req, res) => {
     let routeFile = path.resolve('Vistas/registrer.html');
     res.sendFile(routeFile);
-}); 
+});
 
 app.get('/login', (req, res) => {
     let routeFile = path.resolve('Vistas/login.html');
     res.sendFile(routeFile);
-}); 
+});
 
 // Ruta para archivos
-app.get('*', (req, res) => { 
-    if(req.url.includes('.')){
+app.get('*', (req, res) => {
+    if (req.url.includes('.')) {
         let file = path.resolve('Public' + req.url)
         return res.sendFile(file)
     };
     res.sendFile('404 Not Found');
 })
-//http://localhost:3000/
-app.listen(3000, () => console.log('Server running on port 3000'));
-
+//http://localhost:3030/
+app.listen(3030, () => console.log('Server running on port 3030'));
